318d3b034e3e106b4fb90fcee9c32349edd8e67a
udomday <sunhay@mail.ru>
2023-11-07 21:16:34 +0300

    new style

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,9 @@
+const input = document.querySelector("#gameprice");
+
 async function calcPrice() {
   const response = await fetch("https://www.cbr-xml-daily.ru/latest.js");
   const result = await response.json();
-  const inputValue = document.querySelector("#gameprice").value;
+  const inputValue = input.value;
 
   const KZTPrice =
     inputValue * result.rates.KZT +
@@ -10,5 +12,9 @@ async function calcPrice() {
 
   const answer = KZTPrice / result.rates.KZT;
 
-  document.querySelector("#answer").innerHTML = answer;
+  document.querySelector("#answer").innerHTML = Math.ceil(answer);
 }
+
+input.addEventListener("change", (el) => {
+  calcPrice();
+});
